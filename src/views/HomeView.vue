536692b9a3fc60549488ffa25afd3ae917dcf2d9
Alex <dfattahov@slms.local>
2022-12-05 10:23:33 +0300

    Создал проект

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="1"><v-btn>Главная</v-btn></v-col>
+      <v-col cols="1"><v-btn to="/login">Вход</v-btn></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+
+  components: {},
+});
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="w-100 h-100">
+    <v-row class="w-100 h-100 align-center">
+      <v-col class="justify-center">
+        <v-card class="py-6 px-8 mx-auto" max-width="400">
+          <v-card-title class="text-center mb-5">Вход</v-card-title>
+          <v-form>
+            <v-text-field />
+            <v-text-field />
+            <v-btn>Войти</v-btn>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script></script>
+
+<style></style>
