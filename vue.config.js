c990fb976cf4b55b1a057c5a605808eb703c5664
12twinkle21 <arluncov@yandex.ru>
2022-12-13 16:16:15 +0300

    Настроил прокси

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    proxy: "http://office.diall.ru:19082"
+  },
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
