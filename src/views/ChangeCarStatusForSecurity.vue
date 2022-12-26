794c314b4639f85ba0d42365d9c75ca0a93c079b
12twinkle21 <arluncov@yandex.ru>
2022-12-26 18:22:16 +0300

    Получение статусов заказа.Настройка экрана оператора

--- src/views/ChangeCarStatusForSecurity.vue
+++ src/views/ChangeCarStatusForSecurity.vue
@@ -18,39 +18,44 @@
         </v-list>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="5">
-        <h2 class="mb-5">Тут меняем статус тачки</h2>
-        <v-btn @click="changeTsArrival">ТС прибыло</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="5">
-        <v-btn @click="changeTsOnHold">Отправить ТС на ожидание</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="5">
-        <v-btn class="mr-10" @click="changeCompleteInspection"
-          >ТС Досмотрено</v-btn
-        >
-        <v-btn @click="changeCompleteInspection"
-          >ТС не досмотрено(Отрицание)</v-btn
-        >
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="5">
-        <h2 class="mb-5">Получение статуса(Поле для теста)</h2>
-        <v-btn @click="getStatus">Получить статус</v-btn>
-        <div v-if="getStatusInfo" class="mt-5">
-          <p>
-            Guid_Load: {{ getStatusInfo.map((item) => String(item.GUID_load)) }}
-          </p>
-          <p>Status: {{ getStatusInfo.map((item) => item.Status) }}</p>
-        </div>
-      </v-col>
-    </v-row>
+    <div v-if="group === 'Охрана'">
+      <v-row>
+        <v-col cols="5">
+          <v-btn class="mr-10" @click="changeTsArrival">ТС прибыло</v-btn>
+          <v-btn @click="changeTsOnHold">Отправить ТС на ожидание</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="5">
+          <v-btn class="mr-10" @click="changeCompleteInspection"
+            >ТС Досмотрено</v-btn
+          >
+          <v-btn @click="changeCompleteInspection">ТС не досмотрено</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-if="group === 'Оператор отгрузки'">
+      <v-row>
+        <v-col cols="5">
+          <h2 class="mb-5">Отметка о готовности к погрузке ТС</h2>
+          <v-btn class="mr-10" @click="changeReadyForShipment"
+            >ТС на отгрузку</v-btn
+          >
+          <v-btn @click="changeReadyForShipment">Погрузка ТС невозможна</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="5">
+          <h2 class="mb-5">Опломбирование ТС</h2>
+          <v-text-field v-model="numberOfSeal" label="Номер пломб" />
+          <v-text-field
+            v-model="numberOfSampleSeal"
+            label="Номер пломбы пробы"
+          />
+          <v-btn @click="onSeal">Опломбирование</v-btn>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -67,15 +72,18 @@ export default defineComponent({
     return {
       getTsArrivalInfo: "",
       getTsOnHoldInfo: "",
-      getInspectionInfo: "",
+      getInspectionInfo: [],
       getStatusInfo: "",
+      numberOfSeal: "",
+      numberOfSampleSeal: "",
       guidLoad: this.$route.params.id,
       orderInfo: {},
+      group: localStorage.getItem("Group"),
     };
   },
   mounted() {
     this.getOrder();
-    // this.getStatus();
+    this.getStatus();
   },
   methods: {
     changeTsArrival() {
@@ -87,6 +95,7 @@ export default defineComponent({
       axios
         .get(config.backendUrl, { params })
         .then((response) => (this.getTsArrivalInfo = response.data.data));
+      this.getStatus();
     },
     changeTsOnHold() {
       const params = {
@@ -97,12 +106,13 @@ export default defineComponent({
       axios
         .get(config.backendUrl, { params })
         .then((response) => (this.getTsOnHoldInfo = response.data.data));
+      this.getStatus();
     },
     changeCompleteInspection(item) {
       let inspectionResult = null;
       if (item.target.innerHTML === "ТС Досмотрено") {
         inspectionResult = 0;
-      } else if (item.target.innerHTML === "ТС на ожидании") {
+      } else if (item.target.innerHTML === "ТС не досмотрено") {
         inspectionResult = 1;
       }
       const params = {
@@ -114,6 +124,7 @@ export default defineComponent({
       axios
         .get(config.backendUrl, { params })
         .then((response) => (this.getInspectionInfo = response.data.data));
+      this.getStatus();
     },
     getStatus() {
       const params = {
@@ -135,8 +146,37 @@ export default defineComponent({
         .get(config.backendUrl, { params })
         .then((response) => (this.orderInfo = response.data.data));
     },
+    changeReadyForShipment(item) {
+      if (item.target.innerHTML === "ТС на отгрузку") {
+        this.readyShipmentResult = 1;
+      } else if (item.target.innerHTML === "Погрузка ТС невозможна") {
+        this.readyShipmentResult = 0;
+      }
+      const params = {
+        Request: "ReadyShipment",
+        GUID_Load: this.guidLoad,
+        Result: this.readyShipmentResult,
+      };
+
+      axios
+        .get(config.backendUrl, { params })
+        .then((response) => (this.getShipmentInfo = response.data.data));
+    },
+    onSeal() {
+      const params = {
+        Request: "Sealing",
+        GUID_Load: this.guidLoad,
+        Seal: this.numberOfSeal,
+        SampleSeal: this.numberOfSampleSeal,
+      };
+
+      axios
+        .get(config.backendUrl, { params })
+        .then((response) => (this.getSealingInfo = response.data.data));
+    },
   },
 });
 </script>
+<!-- http://localhost:8080/changeCarStatus/4b0a0636-850c-11ed-9e14-00155d011400 -->
 
 <style></style>
